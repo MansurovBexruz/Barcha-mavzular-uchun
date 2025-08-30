85747d6fc975d98afadcb27fb4f61b7edda547c7
MansurovBexruz <mansurovbexruz21@gmail.com>
2025-08-30 13:48:51 +0500

    Lesson-94 chelange-2 finish codes

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,21 @@
-function getA(callback: (a: number) => void) {
-  console.log("Loading a...");
-  setTimeout(() => {
-    const a = 10;
-    callback(a);
-  }, 1000);
-}
-function getB(callback: (a: number) => void) {
-  console.log("Loading b...");
-  setTimeout(() => {
-    const a = 20;
-    callback(a);
-  }, 1000);
-}
-function getC(callback: (a: number) => void) {
-  console.log("Loading c...");
-  setTimeout(() => {
-    const a = 30;
-    callback(a);
-  }, 1000);
-}
-function getD(callback: (a: number) => void) {
-  console.log("Loading d...");
-  setTimeout(() => {
-    const a = 40;
-    callback(a);
-  }, 1000);
-}
+// function doHomework(hasTime: boolean, isExistHomework: boolean) {
+//   return new Promise((resolve, reject) => {
+//     if (hasTime && isExistHomework)
+//       return resolve(console.log("Homework was done"));
+//   });
+// }
 
-getA((a) => {
-  console.log(`${a}`);
-  getB((b) => {
-    console.log(`${b}`);
-    getC((c) => {
-      console.log(`${c}`);
-      getD((d) => {
-        console.log(`${d}`);
-      });
-    }); 
-  });
-});
+// doHomework(true, true); // Promise<fulfilled>("Homework was done")
 
 /**
- * after 1s : a = 10
- * after 3s : b = 20
- * after 5s : c = 30
- * after 7s : d = 40
- *
+ * doHomework(true, false) // Promise<fulfilled>("Homework was being waited")
+ * doHomework(false, true) // Promise<rejected>("Homework was not done")
  */
+
+try {
+  throw new Error("First");
+} catch (error: any) {
+  if (error instanceof Error) {
+    throw new Error("First");
+  }
+}
