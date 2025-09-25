2d71e83f45ba1238612f411b288eb8770191a930
MansurovBexruz <mansurovbexruz21@gmail.com>
2025-09-25 19:57:00 +0500

    Lesson-104 chellange-2 finish codes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-function connector<T, Z>(a: T, b: Z) {
-  return {
-    a,
-    b
-  };
+// function connector<T, Z>(a: T, b: Z) {
+//   return {
+//     a,
+//     b
+//   };
+// }
+// const a = 10;
+// const b = "pdp-";
+// const result = connector(a, b); // { a: number, b: string }
+// console.log(result);
+
+function greeting<To extends "Kent" | "Mark", From extends "Nico" | "Chico">(to: To, from: From) {
+  return `Hi ${to} from ${from}` as const;
 }
-const a = 10;
-const b = "pdp-";
-const result = connector(a, b); // { a: number, b: string }
-console.log(result);
+
+const message = greeting("Kent", "Chico");
+
+console.log(message);
