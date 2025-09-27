54da80d260c68249a1441cba1e8f86dbd5f5afcc
MansurovBexruz <mansurovbexruz21@gmail.com>
2025-09-27 15:54:13 +0500

    Lesson-106

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <body className="grid place-items-center bg-[#efed40]">
+      <h1 className="text-[100px] text-blue-500 text-center mt-[300px]">Hello world</h1>
+    </body>
+  </StrictMode>
+);
 
 /*
 1. JSX
